c104468c5eb711255db3835b5b8bd50a10c5b9cc
lucindaq <lucinda@merl.us>
2025-02-04 20:24:04 +0000

    Sets up MaterialUI to be used as UI framework

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
+import { List, ListItem, Typography } from "@mui/material";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <img
-          src="/garlick-logo.png"
-          alt="Garlick logo"
-          width={180}
-        />
-        <p>Welcome to Garlick!</p>
+      <Image
+        src="/garlick-logo.png"
+        alt="Garlick logo"
+        width={180} 
+        height={0}
+        style={{ height: "auto" }}
+      />
+      <Typography variant="h2">Welcome to Garlick!</Typography>
+      <List style={{fontSize: 20}}>
+        <ListItem>Caroline Kroll</ListItem>
+        <ListItem>Catherine Lawrence</ListItem>
+        <ListItem>Dayanita Kumar</ListItem>
+        <ListItem>Jennifer Nino</ListItem>
+        <ListItem>Lucinda Quintal</ListItem>
+      </List>
     </div>
   );
 }
