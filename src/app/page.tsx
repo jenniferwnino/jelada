7b64fbe8fe0fe9491ab993e0cdf8b540a70b6b1c
lucindaq <lucinda@merl.us>
2025-02-04 19:54:52 +0000

    Initial commit with React/next.js skeleton

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <img
+          src="/garlick-logo.png"
+          alt="Garlick logo"
+          width={180}
+        />
+        <p>Welcome to Garlick!</p>
+    </div>
+  );
+}
