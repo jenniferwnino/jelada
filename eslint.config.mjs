3316df7e48e8199d9c6999326577c278e8e7e216
lucindaq <lucinda@merl.us>
2025-02-06 12:13:22 +0000

    Sets up eslint rules to keep our code looking beautiful

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettierPlugin from "eslint-plugin-prettier";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      indent: ["error", 2],
+      "react/jsx-indent": ["error", 2],
+      "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
-
-export default eslintConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
       <Image
         src="/garlick-logo.png"
         alt="Garlick logo"
-        width={180} 
+        width={180}
         height={0}
         style={{ height: "auto" }}
       />
       <Typography variant="h2">Welcome to Garlick!</Typography>
-      <List style={{fontSize: 20}}>
+      <List style={{ fontSize: 20 }}>
         <ListItem>Caroline Kroll</ListItem>
         <ListItem>Catherine Lawrence</ListItem>
         <ListItem>Dayanita Kumar</ListItem>
