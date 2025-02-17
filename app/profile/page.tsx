656081dc1e05cf018e8c20bc38440d1fbba2ff31
lucindaq <lucinda@merl.us>
2025-02-17 13:15:09 +0100

    Adds 3 new pages & routes for Profile, Explore Recipes, and specific Recipe Page

---
+++ app/profile/page.tsx
@@ -0,0 +1,9 @@
+import { PageTitle } from "@/components/page-title";
+
+export default function ProfilePage() {
+  return (
+    <>
+      <PageTitle title="Your Profile" subtitle="See your goals & saved recipes!" />
+    </>
+  );
+}
